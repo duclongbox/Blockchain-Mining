fa0d8467ce32b292c8b5945ea1348133e7da09c0
Long Duc Hoang <duclongbox@gmail.com>
2023-09-07 23:09:00 -0400

    validate-chain

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -12,6 +12,34 @@ class Blockchain:
 
     def __repr__(self):
         return f'Blockchain: {self.chain}'
+    
+    def replace_chain(self,chain):
+        """
+        Replace the local chain with the new comming chain:
+        -the incomming chain must be longer than local
+        -the incomming chain must be valid
+        """
+        if len(chain)<=len(self.chain):
+            raise Exception('the replacement must be longer')
+        try: 
+            Blockchain.validate_chain(chain)
+        except Exception as e:
+            print(f'ERROR: {e}')
+        self.chain = chain
+
+    @staticmethod
+    def validate_chain(chain):
+        """
+        Validate the incomming chain must follow:
+        -the chain must start with the genesis block
+        -all the blocks must be valid
+        """
+        if chain[0].__dict__ != Block.genesis().__dict__:
+            raise Exception('the genesis must be valid')
+        for i in range(1,len(chain)):
+            block = chain[i]
+            Block.validate_block(chain[i-1],block)
+
 
 def main():
     blockchain= Blockchain()
