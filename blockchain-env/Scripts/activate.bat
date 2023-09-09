366afbcb74ae92867374415acba9ce4ceca7a4d7
Long Duc Hoang <duclongbox@gmail.com>
2023-09-08 21:01:32 -0400

    flaskAPI

--- blockchain-env/Scripts/activate.bat
+++ blockchain-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\source\blockchain project\blockchain-env
+set VIRTUAL_ENV=C:\Blockchain-Mining\Blockchain-Mining\blockchain-env
 
 if not defined PROMPT set PROMPT=$P$G
 
